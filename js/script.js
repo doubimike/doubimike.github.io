ac00a599e458c6a6f5b52629ec396cf27a39989d
doubimike <doubimike@qq.conm>
2016-03-31 00:18:21 +0800

    设计产品什么的最有意思了。我打算把这份简历做成一个人。

--- js/script.js
+++ js/script.js
@@ -32,16 +32,43 @@ requirejs.config({
 
 requirejs(['jquery', 'fullPage', 'velocity', 'velocityui', 'circleProgress', 'backtop', 'bootstrap', 'jpreLoader'], function($, fullpage, velocity, velocityui, circleProgress, backtop, bootstrap, jpreLoader) {
     $(function() {
-        $('body').jpreLoader({}, init);
-        $('#fullpage').fullpage({
-            sectionsColor: ['#3ABC99', '#5CBEC4', '#81A9BF', '#52C6DA', '#6799B8'],
-            navigation: true,
-            navigationTooltips: ['关于我', '作品', '工作履历'],
-            anchors: ['page1', 'page2', 'page3'],
-            menu: '#fullpageMenu',
-            // fixedElements:'#fullpageMenu',
-            // paddingTop: '50px',
-        });
+            $('body').jpreLoader({}, init);
+            $('#fullpage').fullpage({
+                    sectionsColor: ['#3ABC99', '#5CBEC4', '#81A9BF', '#52C6DA', '#6799B8'],
+                    navigation: true,
+                    navigationTooltips: ['关于我', '作品', '工作履历'],
+                    anchors: ['page1', 'page2', 'page3'],
+                    menu: '#fullpageMenu',
+                    // fixedElements:'#fullpageMenu',
+                    // paddingTop: '50px',
+                    onLeave: function(index, nextIndex, direction) {
+                        var leavingSection = $(this);
+    
+                        //after leaving section 2
+                        if (index == 2 && direction == 'down') {
+                            // alert("Going to section 3!");
+                        } else if (index == 2 && direction == 'up') {
+                            setCircle();
+                        }
+                        
+
+                },
+                afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+            //using index
+            if(index == 1){
+               
+            }
+
+            //using anchorLink
+            if(anchorLink == 'secondSlide'){
+                // alert("Section 2 ended loading");
+            }
+
+
+                    }
+            });
 
         function init() {
             $('#about-me').velocity({
@@ -57,55 +84,7 @@ requirejs(['jquery', 'fullPage', 'velocity', 'velocityui', 'circleProgress', 'ba
                 delay: 100
             });
 
-            $('#circle-html').circleProgress({
-                value: .8
-            }).on('circle-animation-progress', function(event, progress, stepValue) {
-                $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
-            });
-
-            $('#circle-css').circleProgress({
-                value: 0.7,
-                fill: {
-                    gradient: [
-                        '#0681c4', ['#4ac5f8', .5]
-                    ],
-                    gradientAngle: Math.PI / 4
-                }
-            }).on('circle-animation-progress', function(event, progress, stepValue) {
-                $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
-            });
-
-            $('#circle-js').circleProgress({
-                value: 0.6,
-                fill: {
-                    gradient: [
-                        '#3CA1CF', ['#A46A9B', .7]
-                    ],
-                    gradientAngle: Math.PI / 4
-                }
-            }).on('circle-animation-progress', function(event, progress, stepValue) {
-                $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
-            });
-
-            $('#circle-jquery').circleProgress({
-                value: 0.5,
-                fill: {
-                    gradient: [
-                        ['#C6605C', .5],
-                        '#A5BEC3',
-                    ],
-                    gradientAngle: Math.PI / 4
-                }
-            }).on('circle-animation-progress', function(event, progress, stepValue) {
-                $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
-            });
-
-            $('#circle-python').circleProgress({
-                value: 0.4,
-                fill: { gradient: ['#47f', ['#bc4', 0.7]], gradientAngle: Math.PI / 4 }
-            }).on('circle-animation-progress', function(event, progress, stepValue) {
-                $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
-            });
+            setCircle();
 
             new backtop($('#backTop'), {
                 mode: 'move',
@@ -114,6 +93,58 @@ requirejs(['jquery', 'fullPage', 'velocity', 'velocityui', 'circleProgress', 'ba
                 dest: 0
             });
         };
+
+        function setCircle(){
+            $('#circle-html').circleProgress({
+                            value: .8
+                        }).on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
+                        });
+
+                        $('#circle-css').circleProgress({
+                            value: 0.7,
+                            fill: {
+                                gradient: [
+                                    '#0681c4', ['#4ac5f8', .5]
+                                ],
+                                gradientAngle: Math.PI / 4
+                            }
+                        }).on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
+                        });
+
+                        $('#circle-js').circleProgress({
+                            value: 0.6,
+                            fill: {
+                                gradient: [
+                                    '#3CA1CF', ['#A46A9B', .7]
+                                ],
+                                gradientAngle: Math.PI / 4
+                            }
+                        }).on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
+                        });
+
+                        $('#circle-jquery').circleProgress({
+                            value: 0.5,
+                            fill: {
+                                gradient: [
+                                    ['#C6605C', .5],
+                                    '#A5BEC3',
+                                ],
+                                gradientAngle: Math.PI / 4
+                            }
+                        }).on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
+                        });
+
+                        $('#circle-python').circleProgress({
+                            value: 0.4,
+                            fill: { gradient: ['#47f', ['#bc4', 0.7]], gradientAngle: Math.PI / 4 }
+                        }).on('circle-animation-progress', function(event, progress, stepValue) {
+                            $(this).find('strong').html((stepValue * 100).toFixed(0) + '<i>%</i>');
+                        });
+        }
 
     });
 
